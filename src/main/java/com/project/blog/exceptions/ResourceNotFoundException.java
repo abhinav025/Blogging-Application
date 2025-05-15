0a239ac8e660b10f0b597f1ba45f13c894a2a51c
abhinav025 <abhinav.bisht025@gmail.com>
2025-05-15 12:59:09 +0530

    updated message of long

--- src/main/java/com/project/blog/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/project/blog/exceptions/ResourceNotFoundException.java
@@ -12,7 +12,7 @@ public class ResourceNotFoundException extends RuntimeException {
 	long feildValue;
 	
 	public ResourceNotFoundException(String resourceName, String feildName, long feildValue) {
-		super(String.format("%s not found with %s: %l", resourceName,feildName, feildValue));
+		super(String.format("%s not found with %s: %s", resourceName,feildName, feildValue));
 		this.resourceName = resourceName;
 		this.feildName = feildName;
 		this.feildValue = feildValue;
