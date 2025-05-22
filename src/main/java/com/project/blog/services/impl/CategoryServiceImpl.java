96f07bda878e920e71c333ebaa34272f0b811331
abhinav025 <abhinav.bisht025@gmail.com>
2025-05-22 17:42:30 +0530

    completed CategoryServiceImpl

--- src/main/java/com/project/blog/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/project/blog/services/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.project.blog.services.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,20 +41,23 @@ public class CategoryServiceImpl implements CategoryService {
 
 	@Override
 	public void deleteCategory(Integer categoryId) {
-		// TODO Auto-generated method stub
-
+		Category cat = this.categoryRepo.findById(categoryId).orElseThrow(()-> new ResourceNotFoundException("Category ", "Catedory Id", categoryId));
+		this.categoryRepo.delete(cat);
 	}
 
 	@Override
 	public CategoryDto getCategory(Integer categoryId) {
-		// TODO Auto-generated method stub
-		return null;
+		Category cat = this.categoryRepo.findById(categoryId).orElseThrow(()-> new ResourceNotFoundException("Category ", "Catedory Id", categoryId));
+		return this.modelMapper.map(cat, CategoryDto.class);
 	}
 
 	@Override
 	public List<CategoryDto> getCategories() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Category> categories = this.categoryRepo.findAll();
+		List<CategoryDto> catDtos = categories.stream().map((cat)-> this.modelMapper.map(cat, CategoryDto.class)).collect(Collectors.toList());
+		
+		return catDtos;
 	}
 
 }
