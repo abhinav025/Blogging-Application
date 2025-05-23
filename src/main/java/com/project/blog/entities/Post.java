594e9b8a94d9a2b9a377ccc9c027099098ada010
abhinav025 <abhinav.bisht025@gmail.com>
2025-05-23 18:24:05 +0530

    added Post entity

---
+++ src/main/java/com/project/blog/entities/Post.java
@@ -0,0 +1,32 @@
+package com.project.blog.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="posts")
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int postId;
+	
+	@Column(name="post_tille", nullable = false)
+	private String title;
+	
+	@Column(length = 10000)
+	private String content;
+	
+	private String imageName;
+	
+	private Date addedDate;
+	
+
+}
