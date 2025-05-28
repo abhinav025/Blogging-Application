319785a8981cfee63437ced69d7b90d6b428082d
abhinav025 <abhinav.bisht025@gmail.com>
2025-05-28 21:58:30 +0530

    added controller methods

--- src/main/java/com/project/blog/controllers/PostController.java
+++ src/main/java/com/project/blog/controllers/PostController.java
@@ -1,8 +1,12 @@
 package com.project.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,5 +30,22 @@ public class PostController {
 		PostDto createPost = this.postService.createPost(postDto, userId, categoryId);
 		return new ResponseEntity<PostDto>(createPost,HttpStatus.CREATED);
 	}
+	
+	//-- get by userr
+	@GetMapping("/user/{userId}/posts")
+	public ResponseEntity<List<PostDto>> getPostByUser(@PathVariable Integer userId){
+		List<PostDto> posts = this.postService.getPostsByUser(userId);
+		
+		return new ResponseEntity<List<PostDto>>(posts,HttpStatus.OK);
+	}
+	
+	//-- get by catgory
+	@GetMapping("/category/{categoryId}/posts")
+	public ResponseEntity<List<PostDto>> getCategoryByUser(@PathVariable Integer categoryId){
+		List<PostDto> posts = this.postService.getPostsByCategory(categoryId);
+		
+		return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
+	}
+	
 
 }
