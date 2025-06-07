336095eb0d6d0048da0737b422418f8dfc2ccbaa
abhinav025 <abhinav.bisht025@gmail.com>
2025-06-07 23:43:06 +0530

    added properties to upload images

--- src/main/java/com/project/blog/services/impl/FileServiceImpl.java
+++ src/main/java/com/project/blog/services/impl/FileServiceImpl.java
@@ -1,8 +1,12 @@
 package com.project.blog.services.impl;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,7 +16,25 @@ public class FileServiceImpl implements FileService {
 
 	@Override
 	public String uploadImage(String path, MultipartFile file) throws IOException {
-		// TODO Auto-generated method stub
+
+		// -- File name
+		String name = file.getOriginalFilename();
+
+		// -- Full path
+		String filePath = path + File.pathSeparator + name;
+		
+		String randomID = UUID.randomUUID().toString();
+		randomID
+		
+		// -- Create Folder if not created
+		File f = new File(path);
+		if (!f.exists()) {
+			f.mkdir();
+		}
+		
+		//-- File copy
+		Files.copy(file.getInputStream(), Paths.get(filePath));
+		
 		return null;
 	}
 
