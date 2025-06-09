fb8b191d43dae315814f075eafeb3bc2f589c934
abhinav025 <abhinav.bisht025@gmail.com>
2025-06-09 23:30:37 +0530

    added comment options

---
+++ src/main/java/com/project/blog/payloads/CommentDto.java
@@ -0,0 +1,5 @@
+package com.project.blog.payloads;
+
+public class CommentDto {
+
+}
--- src/main/java/com/project/blog/payloads/PostDto.java
+++ src/main/java/com/project/blog/payloads/PostDto.java
@@ -1,8 +1,11 @@
 package com.project.blog.payloads;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.project.blog.entities.Category;
+import com.project.blog.entities.Comment;
 import com.project.blog.entities.User;
 
 import lombok.Getter;
@@ -27,6 +30,8 @@ public class PostDto {
 	private CategoryDto category;
 	
 	private UserDto user;
+	
+	private Set<Comment> comments = new HashSet<>();
 	
 	
 
