8197d3101c41eb6b2201a2b9927951648763c549
abhinav025 <abhinav.bisht025@gmail.com>
2025-06-10 14:42:29 +0530

    completd service and add controller for comments

--- src/main/java/com/project/blog/repositories/CommentRepo.java
+++ src/main/java/com/project/blog/repositories/CommentRepo.java
@@ -1,5 +1,9 @@
 package com.project.blog.repositories;
 
-public interface CommentRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.blog.entities.Comment;
+
+public interface CommentRepo extends JpaRepository<Comment, Integer>{
 
 }
