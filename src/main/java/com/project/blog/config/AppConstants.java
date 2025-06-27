0bbc45460ec000027b72fde692f9e5daa8ddc715
abhinav025 <abhinav.bisht025@gmail.com>
2025-06-27 18:51:46 +0530

    adding spring Security in swagger Configuration

--- src/main/java/com/project/blog/config/AppConstants.java
+++ src/main/java/com/project/blog/config/AppConstants.java
@@ -12,4 +12,5 @@ public class AppConstants {
 	public static final String[] PUBLIC_URLS = { "/api/v1/auth/**", "/v3/api-docs", "/v2/api-docs",
 			"/swagger-resources/**", "/swagger-ui/**", "/webjars/**" };
 
+	public static final String AUTHORIZATION_HEADER="Authorization";
 }
--- src/main/java/com/project/blog/config/SwaggerConfig.java
+++ src/main/java/com/project/blog/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.project.blog.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +11,27 @@ import io.swagger.v3.oas.models.info.Info;
 @Configuration
 public class SwaggerConfig {
 	
+	
+	private Apikey apiKeys() {
+		return new ApiKey("JWT", AppConstants.AUTHORIZATION_HEADER, "header");
+	}
+	
 	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI()
-				.info(new Info()
-						.title("Blog App API")
-						.version("1.0.0")
-						.description("Spring Boot blog application backend using Spring Boot"));
+//	public OpenAPI customOpenAPI() {
+//		return new OpenAPI()
+//				.info(new Info()
+//						.title("Blog App API")
+//						.version("1.0.0")
+//						.description("Spring Boot blog application backend using Spring Boot"));
+//	}
+	public Docket api() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	            .apiInfo(getInfo())
+	            .securityContexts()
+	            .securitySchemes(Arrays.asList(apiKeys))
+	            .select()
+	            .apis(RequestHandlerSelectors.any())
+	            .paths(PathSelectors.any())
+	            .build();
 	}
 }
