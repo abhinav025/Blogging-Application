bf2892c6ccace0a3c29df064ccecde287975f6c2
abhinav025 <abhinav.bisht025@gmail.com>
2025-06-30 23:16:24 +0530

    completed authentication in swagger

--- src/main/java/com/project/blog/config/SwaggerConfig.java
+++ src/main/java/com/project/blog/config/SwaggerConfig.java
@@ -1,43 +1,34 @@
 package com.project.blog.config;
 
-import java.util.Arrays;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 	
+	 public static final String SECURITY_SCHEME_NAME = "JWT";
+
+	    @Bean
+	    public OpenAPI blogOpenAPI() {
+	        return new OpenAPI()
+	                .info(new Info()
+	                        .title("Blog Application API")
+	                        .description("Spring Boot blog application backend using Spring Boot")
+	                        .version("1.0.0"))
+	                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+	                .components(new Components()
+	                        .addSecuritySchemes(SECURITY_SCHEME_NAME, 
+	                            new SecurityScheme()
+	                                .name(SECURITY_SCHEME_NAME)
+	                                .type(SecurityScheme.Type.HTTP)
+	                                .scheme("bearer")
+	                                .bearerFormat("JWT")));
+	    }
 	
-	private Apikey apiKeys() {
-		return new ApiKey("JWT", AppConstants.AUTHORIZATION_HEADER, "header");
-	}
-	
-	@Bean
-//	public OpenAPI customOpenAPI() {
-//		return new OpenAPI()
-//				.info(new Info()
-//						.title("Blog App API")
-//						.version("1.0.0")
-//						.description("Spring Boot blog application backend using Spring Boot"));
-//	}
-	
-	private List<SecurityContext> securityContexts() {
-		return Arrays.asList(SecurityContext.builder().securityReferences(sf()).build());
-	}	
-	
-	
-	public Docket api() {
-	    return new Docket(DocumentationType.SWAGGER_2)
-	            .apiInfo(getInfo())
-	            .securityContexts()
-	            .securitySchemes(Arrays.asList(apiKeys))
-	            .select()
-	            .apis(RequestHandlerSelectors.any())
-	            .paths(PathSelectors.any())
-	            .build();
-	}
 }
--- src/main/java/com/project/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/blog/security/JwtAuthenticationFilter.java
@@ -44,7 +44,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 		
 		String token = null;
 		
-		if(requestToken!=null && requestToken.startsWith("Bearer")) {
+		if(requestToken!=null && requestToken.startsWith("Bearer ")) {
 			
 			token = requestToken.substring(7);
 			
